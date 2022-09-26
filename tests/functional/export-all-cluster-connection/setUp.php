bf797696df7d6a2b66730a2035d395d84fa1fdca
Michal Jurečko <michal.jurecko@gmail.com>
2022-09-26 21:12:34 +0200

    Setup SSL in cluster test

--- tests/functional/export-all-cluster-connection/setUp.php
+++ tests/functional/export-all-cluster-connection/setUp.php
@@ -6,5 +6,9 @@ use MongoExtractor\FunctionalTests\DatadirTest;
 use MongoExtractor\Tests\Traits\ImportDatasetTrait;
 
 return static function (DatadirTest $test): void {
+    putenv('SSL_CA=' . file_get_contents('/tmp/ca-cert.pem'));
+    putenv('SSL_CERT=' . file_get_contents('/tmp/client-cert.pem'));
+    putenv('SSL_KEY=' . file_get_contents('/tmp/client-key.pem'));
+
     (new class { use ImportDatasetTrait; })::importDatatasetCluster('restaurants', 'dataset.json');
 };
