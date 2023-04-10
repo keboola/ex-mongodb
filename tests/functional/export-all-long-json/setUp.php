5acba40f014cbfb67e1787aa648ea3cd0253ca7f
Výborný Adam <mail@adamvyborny.cz>
2023-04-10 13:14:01 +0200

    Failing test with long JSON records

---
+++ tests/functional/export-all-long-json/setUp.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types = 1);
+
+use MongoExtractor\FunctionalTests\DatadirTest;
+use MongoExtractor\Tests\Traits\ImportDatasetTrait;
+
+return static function (DatadirTest $test): void {
+    (new class { use ImportDatasetTrait; })::importDatatasetNoAuthDb(
+        'restaurants',
+        'dataset-with-long-binary-values.json'
+    );
+};
